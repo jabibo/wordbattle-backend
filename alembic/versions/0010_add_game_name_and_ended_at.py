c3b84cf0376950491514192c91592d5292ef1348
Jan Binge <github@binge.de>
2025-06-18 19:52:30 +0200

    fix: Correct migration revision chain for game name and ended_at fields - Update revision ID to link to latest migration 63f920666b37 - This will ensure Alembic applies the migration during next deployment

--- alembic/versions/0010_add_game_name_and_ended_at.py
+++ alembic/versions/0010_add_game_name_and_ended_at.py
@@ -1,7 +1,7 @@
 """Add name and ended_at fields to Game model for contract compliance
 
-Revision ID: 0010
-Revises: 0009_add_user_language_and_last_login
+Revision ID: 0010_add_game_name_and_ended_at
+Revises: 63f920666b37
 Create Date: 2024-12-18 14:20:00.000000
 
 """
@@ -10,8 +10,8 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '0010'
-down_revision = '0009'
+revision = '0010_add_game_name_and_ended_at'
+down_revision = '63f920666b37'
 branch_labels = None
 depends_on = None
 
